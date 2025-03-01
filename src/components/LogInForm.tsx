f8f8ccf7bddadecdb109f1c52e13f6b936d6090e
tbsoysal <berksysl.g@gmail.com>
2025-03-01 21:28:06 +0300

    add remember me toggler

--- src/components/LogInForm.tsx
+++ src/components/LogInForm.tsx
@@ -5,6 +5,15 @@ function LogInForm() {
     <form>
       <InputBox textField={"Email"} inputType={"email"} />
       <InputBox textField={"Password"} inputType={"password"} />
+      <p className="rememberText">
+        Remember me{" "}
+        <span
+          className="toggleContainer"
+          onClick={(e) => e.currentTarget.classList.toggle("off")}
+        >
+          <span className="circle"></span>
+        </span>
+      </p>
     </form>
   );
 }
